774484428c51d68e870548c9c1b931de04c5daac
rohithvijayan <rohithvij20@gmail.com>
2022-06-12 20:44:43 +0530

    Added Gifs to corresponding weather condition

--- bot_responses.py
+++ bot_responses.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 import requests
+
+
 # def response(text_input):
 #     user_mssg=str(text_input).lower()
 #     if user_mssg in('hi','hello','sup','hey'):
@@ -23,18 +24,18 @@ import requests
 #     return "Sorry,didnt get you"
 
 def weather(city):
-    city_name=str(city).capitalize()
+    city_name = str(city).capitalize()
     api_key = "a833da2ea0ffccd99997fa69e7cf0f81"
     url = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + city + "&appid=" + api_key
     weather_data = requests.get(url)
     weather_condition = weather_data.json()['weather'][0]['main']
     description = weather_data.json()['weather'][0]['description']
     temp_kelvin = round(weather_data.json()['main']['temp'])
-    temp_faren=round(1.8*(temp_kelvin-273) + 32)
+    temp_faren = round(1.8 * (temp_kelvin - 273) + 32)
     temp_min_kelv = round(weather_data.json()['main']['temp_min'])
-    temp_min_faren=round(1.8*(temp_min_kelv-273) + 32)
+    temp_min_faren = round(1.8 * (temp_min_kelv - 273) + 32)
     temp_max_kelv = round(weather_data.json()['main']['temp_max'])
-    temp_max_faren=round(1.8*(temp_max_kelv-273) + 32)
+    temp_max_faren = round(1.8 * (temp_max_kelv - 273) + 32)
     humidity = weather_data.json()['main']['humidity']
     # print(f'The Weather Conditions In {city_name} Is:')
     # print('-------------------------------------------')
@@ -43,7 +44,10 @@ def weather(city):
     # print('Minimum Temperature:' + str(temp_min) + '°F')
     # print('Maximum Temperature:' + str(temp_max) + '°F')
     # print('Humidity:' + str(humidity) + '%')
-    result=('The Weather Conditions In '+city_name+'\n'+'Weather :' + weather_condition + '--' + description+ '\n'+'Temperature:'+str(temp_faren)+"°F"+'\n'+'Minimum Temperature:' + str(temp_min_faren) +
-            '°F'+'\n'+'Maximum Temperature:' + str(temp_max_faren) + '°F'+'\n'+'Humidity:' + str(humidity) + '%')
+    result = (
+                'The Weather Conditions In ' + city_name + '\n' + 'Weather :' + weather_condition + '\n' + 'Temperature:' + str(
+            temp_faren) + "°F" + '\n' + 'Minimum Temperature:' + str(temp_min_faren) +
+                '°F' + '\n' + 'Maximum Temperature:' + str(temp_max_faren) + '°F' + '\n' + 'Humidity:' + str(
+            humidity) + '%')
     print(result)
     return result
--- main.py
+++ main.py
@@ -19,10 +19,33 @@ def help_command(update, context):
 #     response = R.response(text)
 #     update.message.reply_text(response)
 
-def handle_weather(update,context):
-    city=str(update.message.text).lower()
-    weather_data=R.weather(city)
-    update.message.reply_text(weather_data)
+def handle_weather(update, context):
+    city = str(update.message.text).lower()
+    weather_data = R.weather(city)
+    if 'Clear' in weather_data:
+        update.message.reply_text(weather_data)
+        update.message.reply_animation("https://tenor.com/bGZYe.gif", 'Animation')
+    elif any(a in weather_data for a in ('Haze','Mist','Fog')):
+        update.message.reply_text(weather_data)
+        update.message.reply_animation("https://tenor.com/0BRE.gif", 'Animation')
+    elif 'Cloud' in  weather_data:
+        update.message.reply_text(weather_data)
+        update.message.reply_animation("https://tenor.com/bSAfU.gif", 'Animation')
+    elif any(c in weather_data for c in ('Rain','Shower','Drizzle')):
+        update.message.reply_text(weather_data)
+        update.message.reply_animation("https://tenor.com/N2OK.gif", 'Animation')
+    elif'Thunderstorm' in weather_data:
+        update.message.reply_text(weather_data)
+        update.message.reply_animation("https://tenor.com/bdzkS.gif", 'Animation')
+    elif any(e in weather_data for e in ('Snow', 'Sleet')):
+        update.message.reply_text(weather_data)
+        update.message.reply_animation("https://tenor.com/37Ql.gif", 'Animation')
+    elif 'Sun' in weather_data:
+        update.message.reply_text(weather_data)
+        update.message.reply_animation("blob:https://tenor.com/492013a0-e176-4f35-a4f6-fa33dd7bd260", 'Animation')
+    elif 'Dust' in weather_data:
+        update.message.reply_text(weather_data)
+        update.message.reply_animation("https://tenor.com/bKOXg.gif", 'Animation')
 
 def error(update, context):
     print(f"update{update}caused error{context.error}")
@@ -34,7 +57,7 @@ def main():
     dp.add_handler(CommandHandler('start', start_command))
     dp.add_handler(CommandHandler('help', help_command))
     # dp.add_handler(MessageHandler(Filters.text, handle_message))
-    dp.add_handler(MessageHandler(Filters.text,handle_weather))
+    dp.add_handler(MessageHandler(Filters.text, handle_weather))
     dp.add_error_handler(error)
     updater.start_polling(0)
     updater.idle()
